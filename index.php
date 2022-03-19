6355297df6e75912566c66704df1e1c0fc371f2a
karim076 <59794989+karim076@users.noreply.github.com>
2022-03-19 12:37:39 +0100

    Added basic html structure

---
+++ index.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="nl">
+
+<head>
+    <?php require_once 'head.php'; ?>
+</head>
+
+<body>
+    <header>
+        <?php require_once 'header.php'; ?>
+    </header>
+    <main></main>
+    <footer>
+        <?php require_once 'footer.php'; ?>
+    </footer>
+</body>
+
+</html>
