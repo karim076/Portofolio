27002ec5c9265d34e4b18586e1fa03bc1ceaaa06
karim076 <59794989+karim076@users.noreply.github.com>
2022-03-21 23:39:45 +0100

    Added header the header

---
+++ header.php
@@ -0,0 +1,7 @@
+<div class="wrapper">
+	<div class="leftside">
+		<img class="icon" src="img/Portofolio_Icon.png" alt="Portofolio">
+		<h1>Portofolio</h1>
+	</div>
+	<div class="right side"></div>
+</div>
