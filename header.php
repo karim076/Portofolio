5c52e29d415dd9c4e71d36382e6d1ba6d17b33f1
karim076 <59794989+karim076@users.noreply.github.com>
2022-03-23 20:54:50 +0100

    Added header/section/Javascript
    header: added a portofolio
    Section: all png;s added="moon.mountainbehind,
    mountainafter"
    Javascript: added animation slide for the png's and text

--- header.php
+++ header.php
@@ -1,7 +1,38 @@
-<div class="wrapper">
-	<div class="leftside">
-		<img class="icon" src="img/Portofolio_Icon.png" alt="Portofolio">
-		<h1>Portofolio</h1>
-	</div>
-	<div class="right side"></div>
-</div>
+<header>
+	<a href="#" class="logo">Portofolio</a>
+	<ul>
+		<li><a href="" class="active">Home</a></li>
+		<li><a href="">About me</a></li>
+		<li><a href="">Projects</a></li>
+		<li><a href="">Contact</a></li>
+	</ul>
+</header>
+<section>
+	<img src="img/stars.png" id="stars">
+	<img src="img/moon.png" id="moon">
+	<img src="img/mountains_behind.png" id="mountains_behind">
+	<h2 id="text">Karim</h2>
+	<a href="#sec" id="btn">Adventuur</a>
+	<img src="img/mountains_front.png" id="mountains_front">
+</section>
+<!-- JavaScript -> movement van top naar rechts en links -->
+<script type="text/javascript">
+	let stars = document.getElementById('stars');
+	let moon = document.getElementById('moon'); 
+	let mountains_behind = document.getElementById('mountains_behind');
+	let text = document.getElementById('text');
+	let btn = document.getElementById('btn');
+	let mountains_front = document.getElementById('mountains_front');
+	let header = document.quarySelector('header');
+	
+	window.addEventListener('scroll', function(){
+		let value = window.scrollY;
+		stars.style.left = value + 0.25 + 'px';
+		moon.style.top = value + 1.05 + 'px';
+		mountains_behind.style.top = value + 0.05 + 'px';
+		text.style.marginRight = value + 8 + 'px';
+		text.style.marginTop = value + 1.5 + 'px';
+		btn.style.marginTop = value + 1.5 + 'px';
+		header.style.style.top = value + 0.05 + 'px';		
+	}); 
+</script>
