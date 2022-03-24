e3529e3503abf543aa9efd65365b460a16b7b2f7
karim076 <59794989+karim076@users.noreply.github.com>
2022-03-24 09:41:20 +0100

    Added alt's for img and fixed quary to query

--- header.php
+++ header.php
@@ -8,12 +8,12 @@
 	</ul>
 </header>
 <section>
-	<img src="img/stars.png" id="stars">
-	<img src="img/moon.png" id="moon">
-	<img src="img/mountains_behind.png" id="mountains_behind">
+	<img src="img/stars.png" id="stars" alt="stars">
+	<img src="img/moon.png" id="moon" alt="moon">
+	<img src="img/mountains_behind.png" id="mountains_behind" alt="mountains_behind">
 	<h2 id="text">Karim</h2>
 	<a href="#sec" id="btn">Adventuur</a>
-	<img src="img/mountains_front.png" id="mountains_front">
+	<img src="img/mountains_front.png" id="mountains_front" alt="mountains_front">
 </section>
 <!-- JavaScript -> movement van top naar rechts en links -->
 <script type="text/javascript">
@@ -23,7 +23,7 @@
 	let text = document.getElementById('text');
 	let btn = document.getElementById('btn');
 	let mountains_front = document.getElementById('mountains_front');
-	let header = document.quarySelector('header');
+	let header = document.querySelector('header');
 	
 	window.addEventListener('scroll', function(){
 		let value = window.scrollY;
