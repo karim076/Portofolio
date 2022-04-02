61209a8bd485fbacf3317790637a9b7383397a77
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-02 21:25:42 +0200

    Added wrapper, cols, row, box for nice layout

--- index.php
+++ index.php
@@ -8,15 +8,58 @@
 <body>
 	<?php require_once 'header.php'; ?>
 	<main>
-		<div class="sec" id="sec">
+		<div class="sec about">
 			<h2>Over mij</h2>
 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit delectus molestias nihil aliquam facere atque excepturi ut, consequatur placeat labore, molestiae. Dolorum, non. Repudiandae quibusdam impedit quo esse ex molestiae commodi autem explicabo? Minus, expedita, repellat laborum necessitatibus eos cumque. Eos, quas atque inventore pariatur, illo, quidem incidunt provident veritatis laborum necessitatibus vero beatae adipisci eveniet aliquid, quisquam. Dolorum quidem est quis iste commodi architecto ullam suscipit maxime necessitatibus, error facilis vitae odit reiciendis eius dolorem eum perspiciatis pariatur<br><br> ducimus porro minima explicabo sit ea. Iure, sapiente quibusdam, nam totam harum doloremque voluptas assumenda, optio iste voluptatibus nemo libero? Quia, unde, neque. Eveniet, autem, quod! Unde ea, repudiandae? Iure reiciendis necessitatibus quo dolore consequuntur iste nesciunt ad eaque consequatur exercitationem autem aspernatur sit itaque iusto, temporibus perferendis recusandae atque nisi dolor facere sed, esse. Sequi labore, voluptatibus quisquam porro obcaecati consequatur, suscipit non quos maxime cupiditate earum odio id qui.<br><br></p>
 		</div>
-		<div class="sec">
+		<div class="sec project">
 			<h2>Projecten</h2>
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit delectus molestias nihil aliquam facere atque excepturi ut, consequatur placeat labore, molestiae. Dolorum, non. Repudiandae quibusdam impedit quo esse ex molestiae commodi autem explicabo? Minus, expedita, repellat laborum necessitatibus eos cumque. Eos, quas atque inventore pariatur, illo, quidem incidunt provident veritatis laborum necessitatibus vero beatae adipisci eveniet aliquid, quisquam. Dolorum quidem est quis iste commodi architecto ullam suscipit maxime necessitatibus, error facilis vitae odit reiciendis eius dolorem eum perspiciatis pariatur<br><br> ducimus porro minima explicabo sit ea. Iure, sapiente quibusdam, nam totam harum doloremque voluptas assumenda, optio iste voluptatibus nemo libero? Quia, unde, neque. Eveniet, autem, quod! Unde ea, repudiandae? Iure reiciendis necessitatibus quo dolore consequuntur iste nesciunt ad eaque consequatur exercitationem autem aspernatur sit itaque iusto, temporibus perferendis recusandae atque nisi dolor facere sed, esse. Sequi labore, voluptatibus quisquam porro obcaecati consequatur, suscipit non quos maxime cupiditate earum odio id qui.<br><br>
+			<div class="wrapper">
+				<div class="col1"><!-- colum 1 -->
+					<div class="row">
+						<div class="box">
+							<a target="_blank" rel="noopener noreferrer" href="https://github.com/karim076/H13_Timesheet"><img src="img/project/timesheet.png"></a>
+							<p><i class="fab fa-github space"></i>Project: H13 Timesheet</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="box">
+							<a target="_blank" rel="noopener noreferrer" href="https://github.com/karim076/H13_Timesheet"><img src="img/project/timesheet.png"></a>
+							<p><i class="fab fa-github space"></i>Project: H13 Timesheet</p>
+						</div>
+					</div>
+					<div class="row">
+						<div class="box">
+							<a target="_blank" rel="noopener noreferrer" href="https://github.com/karim076/H13_Timesheet"><img src="img/project/timesheet.png"></a>
+							<p><i class="fab fa-github space"></i>Project: H13 Timesheet</p>
+						</div>
+					</div>				
+				</div><!-- colum 2 -->	
+				<div class="col1">
+					<div class="row">
+						<div class="box">
+							<a target="_blank" rel="noopener noreferrer" href="https://github.com/karim076/H13_Timesheet"><img src="img/project/timesheet.png"></a>
+							<p><i class="fab fa-github space"></i>Project: H13 Timesheet</p>
+						</div>
+					</div>	
+					<div class="row">
+						<div class="box">
+							<a target="_blank" rel="noopener noreferrer" href="https://github.com/karim076/H13_Timesheet"><img src="img/project/timesheet.png"></a>
+							<p><i class="fab fa-github space"></i>Project: H13 Timesheet</p>
+						</div>
+					</div>	
+					<div class="row">
+						<div class="box">
+							<a target="_blank" rel="noopener noreferrer" href="https://github.com/karim076/H13_Timesheet"><img src="img/project/timesheet.png"></a>
+							<p><i class="fab fa-github space"></i>Project: H13 Timesheet</p>
+						</div>
+					</div>			
+				</div>
+			</div>
+
+<!-- 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit delectus molestias nihil aliquam facere atque excepturi ut, consequatur placeat labore, molestiae. Dolorum, non. Repudiandae quibusdam impedit quo esse ex molestiae commodi autem explicabo? Minus, expedita, repellat laborum necessitatibus eos cumque. Eos, quas atque inventore pariatur, illo, quidem incidunt provident veritatis laborum necessitatibus vero beatae adipisci eveniet aliquid, quisquam. Dolorum quidem est quis iste commodi architecto ullam suscipit maxime necessitatibus, error facilis vitae odit reiciendis eius dolorem eum perspiciatis pariatur<br><br> ducimus porro minima explicabo sit ea. Iure, sapiente quibusdam, nam totam harum doloremque voluptas assumenda, optio iste voluptatibus nemo libero? Quia, unde, neque. Eveniet, autem, quod! Unde ea, repudiandae? Iure reiciendis necessitatibus quo dolore consequuntur iste nesciunt ad eaque consequatur exercitationem autem aspernatur sit itaque iusto, temporibus perferendis recusandae atque nisi dolor facere sed, esse. Sequi labore, voluptatibus quisquam porro obcaecati consequatur, suscipit non quos maxime cupiditate earum odio id qui.<br><br>
 				Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit delectus molestias nihil aliquam facere atque excepturi ut, consequatur placeat labore, molestiae. Dolorum, non. Repudiandae quibusdam impedit quo esse ex molestiae commodi autem explicabo? Minus, expedita, repellat laborum necessitatibus eos cumque. Eos, quas atque inventore pariatur, illo, quidem incidunt provident veritatis laborum necessitatibus vero beatae adipisci eveniet aliquid, quisquam. Dolorum quidem est quis iste commodi architecto ullam suscipit maxime necessitatibus, error facilis vitae odit reiciendis eius dolorem eum perspiciatis pariatur<br><br> ducimus porro minima explicabo sit ea. Iure, sapiente quibusdam, nam totam harum doloremque voluptas assumenda, optio iste voluptatibus nemo libero? Quia, unde, neque. Eveniet, autem, quod! Unde ea, repudiandae? Iure reiciendis necessitatibus quo dolore consequuntur iste nesciunt ad eaque consequatur exercitationem autem aspernatur sit itaque iusto, temporibus perferendis recusandae atque nisi dolor facere sed, esse. Sequi labore, voluptatibus quisquam porro obcaecati consequatur, suscipit non quos maxime cupiditate earum odio id qui.<br><br>
-			</p>
+			</p> -->
 		</div>	
 	</main>
 	<?php require_once 'footer.php'; ?>
