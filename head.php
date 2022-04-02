d115eb2a164a7b92f5f75b85287a93f3098a379b
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-02 19:37:57 +0200

    Added fontawesome to head.php for icons

--- head.php
+++ head.php
@@ -2,4 +2,5 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" type="text/css" href="css/main.css">
 <link rel="stylesheet" type="text/css" href="css/footer.css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.1/css/all.css">
 <title>Portofolio</title>
