7f862c516ce76f8370c17341fb77866b2b0724da
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-03 01:59:14 +0200

    Added pictures and new id,classes and start of contact us

--- index.php
+++ index.php
@@ -8,11 +8,11 @@
 <body>
 	<?php require_once 'header.php'; ?>
 	<main>
-		<div class="sec about">
+		<div class="sec about" id="aboutus">
 			<h2>Over mij</h2>
 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit delectus molestias nihil aliquam facere atque excepturi ut, consequatur placeat labore, molestiae. Dolorum, non. Repudiandae quibusdam impedit quo esse ex molestiae commodi autem explicabo? Minus, expedita, repellat laborum necessitatibus eos cumque. Eos, quas atque inventore pariatur, illo, quidem incidunt provident veritatis laborum necessitatibus vero beatae adipisci eveniet aliquid, quisquam. Dolorum quidem est quis iste commodi architecto ullam suscipit maxime necessitatibus, error facilis vitae odit reiciendis eius dolorem eum perspiciatis pariatur<br><br> ducimus porro minima explicabo sit ea. Iure, sapiente quibusdam, nam totam harum doloremque voluptas assumenda, optio iste voluptatibus nemo libero? Quia, unde, neque. Eveniet, autem, quod! Unde ea, repudiandae? Iure reiciendis necessitatibus quo dolore consequuntur iste nesciunt ad eaque consequatur exercitationem autem aspernatur sit itaque iusto, temporibus perferendis recusandae atque nisi dolor facere sed, esse. Sequi labore, voluptatibus quisquam porro obcaecati consequatur, suscipit non quos maxime cupiditate earum odio id qui.<br><br></p>
 		</div>
-		<div class="sec project">
+		<div class="sec project" id="project">
 			<h2>Projecten</h2>
 			<div class="wrapper">
 				<div class="col1"><!-- colum 1 -->
@@ -24,37 +24,86 @@
 					</div>
 					<div class="row">
 						<div class="box">
-							<a target="_blank" rel="noopener noreferrer" href="https://github.com/karim076/H13_Timesheet"><img src="img/project/timesheet.png"></a>
-							<p><i class="fab fa-github space"></i>Project: H13 Timesheet</p>
+							<a target="_blank" rel="noopener noreferrer" href="https://github.com/karim076/H8_prikbord"><img src="img/project/prik.png"></a>
+							<p><i class="fab fa-github space"></i>Project: H8 Prikbord</p>
 						</div>
 					</div>
 					<div class="row">
 						<div class="box">
-							<a target="_blank" rel="noopener noreferrer" href="https://github.com/karim076/H13_Timesheet"><img src="img/project/timesheet.png"></a>
-							<p><i class="fab fa-github space"></i>Project: H13 Timesheet</p>
+							<a target="_blank" rel="noopener noreferrer" href="https://github.com/karim076/H7_DierenTuin"><img src="img/project/dierentuin.png"></a>
+							<p><i class="fab fa-github space"></i>Project: H7 Dierentuin CSV</p>
 						</div>
 					</div>				
 				</div><!-- colum 2 -->	
 				<div class="col1">
 					<div class="row">
 						<div class="box">
-							<a target="_blank" rel="noopener noreferrer" href="https://github.com/karim076/H13_Timesheet"><img src="img/project/timesheet.png"></a>
-							<p><i class="fab fa-github space"></i>Project: H13 Timesheet</p>
+							<a target="_blank" rel="noopener noreferrer" href="https://github.com/karim076/Flappy-Bird-AI-Game_NN"><img src="img/project/flappy.png"></a>
+							<p><i class="fab fa-github space"></i>Project: Flappy Birds AI playing</p>
 						</div>
 					</div>	
 					<div class="row">
 						<div class="box">
-							<a target="_blank" rel="noopener noreferrer" href="https://github.com/karim076/H13_Timesheet"><img src="img/project/timesheet.png"></a>
-							<p><i class="fab fa-github space"></i>Project: H13 Timesheet</p>
+							<a target="_blank" rel="noopener noreferrer" href="https://github.com/karim076/Maze-BFS"><img src="img/project/bfs.jpg"></a>
+							<p><i class="fab fa-github space"></i>Project: Maze AI Breadth First Search</p>
 						</div>
 					</div>	
 					<div class="row">
 						<div class="box">
-							<a target="_blank" rel="noopener noreferrer" href="https://github.com/karim076/H13_Timesheet"><img src="img/project/timesheet.png"></a>
-							<p><i class="fab fa-github space"></i>Project: H13 Timesheet</p>
+							<a target="_blank" rel="noopener noreferrer" href="https://github.com/karim076/Neural-Network_Image_Rec"><img src="img/project/digit_rec.png"></a>
+							<p><i class="fab fa-github space"></i>Project: Digit recognitiont</p>
 						</div>
 					</div>			
 				</div>
+			</div>
+		</div>
+		<div class="sec contact" id="contactme">
+			<div class="wrapper">
+				<form>
+					<div class="card">
+						<h2>Contact Us</h2>
+						<div class="row">
+							<div class="col">
+								<div class="form-group">
+									<label>First Name</label>
+									<input type="text">
+								</div>
+							</div>
+
+							<div class="col">
+								<div class="form-group">
+									<label>Surname</label>
+									<input type="text">
+								</div>
+							</div>
+
+							<div class="col">
+								<div class="form-group">
+									<label>Email</label>
+									<input type="text">
+								</div>
+							</div>
+
+							<div class="col">
+								<div class="form-group">
+									<label>Phone</label>
+									<input type="text">
+								</div>
+							</div>
+
+							<div class="col">
+								<div class="form-group">
+									<label>Message</label>
+									<textarea></textarea>
+								</div>
+							</div>
+
+							<div class="col">
+								<input type="submit" value="Submit">
+							</div>
+						</div>
+					</div>
+				</form>
 			</div>
 		</div>	
 	</main>
