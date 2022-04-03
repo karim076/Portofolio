ee36a61d08c3375cb4530e57f7af86c43cdaa7e1
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-04 01:54:41 +0200

    Added html about me

--- index.php
+++ index.php
@@ -12,17 +12,61 @@
 			<img class="pfp" src="img/pfp/pfp.jpg">
 			<h2>About me</h2>
 			<div class="table">
-				<div class="column">
+				<div class="column1">
 					<h4>Who am i</h4>
+					<p>👨‍🎓I am a student at Radius college, where i follow software development.<br>
+						💻 I can code in => Python, C#, PHP, html, CSS,<br>
+						📚I'd love to learn more about Deep AI, made already some ML<br> 
+						with Neural networks.<br>
+						🌱I also like to make website's because, i can show my creativity visualy.<br>
+						## Topical<br>
+						- 🌱 I’m currently learning Deep AI ,<br>also learning about unity engine wich is really fun to do.<br>
+						- I also have a designer background, i did alot of photoshoping in Adobe Photoshop and video editing,<br>but my preference is more in coding but its a nice skill to have.<br>
+
+					</p>
 				</div>
 				<div class="column">
 					<h4>Coding</h4>
-				</div>
-				<div class="column">
-					<h4>Design</h4>
-				</div>
+					<p>
+						Sins i was 15 i started coding in python, i builded my first application wich was a quiz game.<br>
+						Sins then i improved alot and got to learn new programming language's and what the differences are between them.<br>
+						<span>🛠skill🛠</span><br>
+						<td> <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/html5/html5-original.svg" alt="HTML5" width="60px" title="HTML5"/> </td>
+						<td> <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/css3/css3-original.svg" alt="CSS3" width="60px" title="CSS3"/> </td>
+						<td> <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/javascript/javascript-original.svg" alt="JavaScript" width="60px" title="JavaScript"/> </td>
+						<td> <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/jquery/jquery-plain-wordmark.svg" alt="jQuery" width="60px" title="jQuery"/> </td>
+						<td> <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/php/php-original.svg" alt="PHP" width="60px" title="PHP"/> </td>
+						<td> <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/mysql/mysql-original-wordmark.svg" alt="MySQL" width="60px" title="MySQL"/> </td>
+						<td rowspan="2">
+						</td>
+					</tr>
+
+					<tr>
+						<td> <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/csharp/csharp-original.svg" alt="C#" width="60px" title="C#"/> </td>
+						<td> <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/python/python-original.svg" alt="Python" width="60px" title="Python"/> </td>
+						<td> <img src="https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/bootstrap/bootstrap.png" alt="ThreeJS" width="60px" title="Bootstrap"/> </td>
+						<td> <img src="https://raw.githubusercontent.com/devicons/devicon/1119b9f84c0290e0f0b38982099a2bd027a48bf1/icons/vscode/vscode-original.svg" alt="Visual Studio Code" width="60px" title="Visual Studio Code"/> </td>
+					</tr>
+				</p>
 			</div>
-			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit delectus molestias nihil aliquam facere atque excepturi ut, consequatur placeat labore, molestiae. Dolorum, non. Repudiandae quibusdam impedit quo esse ex molestiae commodi autem explicabo? Minus, expedita, repellat laborum necessitatibus eos cumque. Eos, quas atque inventore pariatur, illo, quidem incidunt provident veritatis laborum necessitatibus vero beatae adipisci eveniet aliquid, quisquam. Dolorum quidem est quis iste commodi architecto ullam suscipit maxime necessitatibus, error facilis vitae odit reiciendis eius dolorem eum perspiciatis pariatur<br><br> ducimus porro minima explicabo sit ea. Iure, sapiente quibusdam, nam totam harum doloremque voluptas assumenda, optio iste voluptatibus nemo libero? Quia, unde, neque. Eveniet, autem, quod! Unde ea, repudiandae? Iure reiciendis necessitatibus quo dolore consequuntur iste nesciunt ad eaque consequatur exercitationem autem aspernatur sit itaque iusto, temporibus perferendis recusandae atque nisi dolor facere sed, esse. Sequi labore, voluptatibus quisquam porro obcaecati consequatur, suscipit non quos maxime cupiditate earum odio id qui.<br><br></p>
+			<div class="column">
+				<h4>Design</h4>
+				<p>
+					When i entered high school i had a choice to look for a specific industry,<br>
+					where i choose ICT, the things i learned where mostly design skills like editing video's,
+					photoshoping and 3d modeling and a little bit of html and css.<br><br>
+					## Design skills<br>
+					<img src="https://img.icons8.com/color/344/adobe-photoshop--v1.png" width="80px" title="Adobe Photoshop"/>
+					<img src="https://img.icons8.com/color/344/adobe-illustrator--v1.png" width="80px" title="Adobe AI"/>
+					<img src="https://img.icons8.com/color/344/adobe-xd--v1.png" width="80px" title="Adobe XD"/><br>
+
+					<img src="https://img.icons8.com/color/344/adobe-premiere-pro--v1.png" width="80px" title="Adobe Premerie Pro"/>
+					<img src="https://img.icons8.com/color/344/google-sketchup.png" width="80px" title="Sketchup"/>
+					<img src="https://img.icons8.com/color/344/blender-3d.png" width="80px" title="Blender"/><br>
+				</p>
+			</div>
+		</div>
+		<img src="https://github-readme-stats.vercel.app/api/top-langs/?username=karim076&layout=compact&hide=typescript&theme=dark&langs_count=5"/>
 		</div>
 		<div class="sec project" id="project">
 			<h2>Projecten</h2>
@@ -76,7 +120,7 @@
 			</div>
 		</div>	
 	</main>
-	<?php require_once 'footer.php'; ?>
+<?php require_once 'footer.php'; ?>
 </body>
 
 </html>
