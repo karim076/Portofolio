f3105572223220577b20b4c51af395aaacd95428
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-03 14:24:38 +0200

    English text

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	<?php require_once 'header.php'; ?>
 	<main>
 		<div class="sec about" id="aboutus">
-			<h2>Over mij</h2>
+			<h2>About me</h2>
 			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit delectus molestias nihil aliquam facere atque excepturi ut, consequatur placeat labore, molestiae. Dolorum, non. Repudiandae quibusdam impedit quo esse ex molestiae commodi autem explicabo? Minus, expedita, repellat laborum necessitatibus eos cumque. Eos, quas atque inventore pariatur, illo, quidem incidunt provident veritatis laborum necessitatibus vero beatae adipisci eveniet aliquid, quisquam. Dolorum quidem est quis iste commodi architecto ullam suscipit maxime necessitatibus, error facilis vitae odit reiciendis eius dolorem eum perspiciatis pariatur<br><br> ducimus porro minima explicabo sit ea. Iure, sapiente quibusdam, nam totam harum doloremque voluptas assumenda, optio iste voluptatibus nemo libero? Quia, unde, neque. Eveniet, autem, quod! Unde ea, repudiandae? Iure reiciendis necessitatibus quo dolore consequuntur iste nesciunt ad eaque consequatur exercitationem autem aspernatur sit itaque iusto, temporibus perferendis recusandae atque nisi dolor facere sed, esse. Sequi labore, voluptatibus quisquam porro obcaecati consequatur, suscipit non quos maxime cupiditate earum odio id qui.<br><br></p>
 		</div>
 		<div class="sec project" id="project">
