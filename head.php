5c4f4ea1af49c4cf1386799d687c878973e52cb3
karim076 <59794989+karim076@users.noreply.github.com>
2022-04-07 10:03:28 +0200

    Added base url

--- head.php
+++ head.php
@@ -1,8 +1,9 @@
+<?php require_once 'backend/conn.php' ?>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link href='https://fonts.googleapis.com/css?family=Ubuntu' rel='stylesheet' type='text/css'/>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<link rel="stylesheet" type="text/css" href="css/main.css">
-<link rel="stylesheet" type="text/css" href="css/footer.css">
+<link rel="stylesheet" type="text/css" href="<?php $base_url ?>css/main.css">
+<link rel="stylesheet" type="text/css" href="<?php $base_url ?>css/footer.css">
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.1.1/css/all.css">
 <title>Portofolio</title>
